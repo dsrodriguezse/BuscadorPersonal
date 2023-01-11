22da6317f71cdb65122a5b05e823cccf420b744b
dsrodriguezse <dsrodriguezse@unal.edu.co>
2023-01-10 20:37:52 -0500

    nombramiento de clases por componente

--- buscador-personal/src/views/Search/components/SearchBox/index.js
+++ buscador-personal/src/views/Search/components/SearchBox/index.js
@@ -1,13 +1,18 @@
 import {useState} from "react"
+import "./style.css"
 
 export default function SearchBox(){
     const [searchText, setSearchText] = useState("")
 
     return(
-        <div>
-            Buscador de personal
+        <div className="search-box">
+            <h2 className="search-box-title">Buscador de personal</h2>
             <label>
-                <input value = {searchText} onChange = {({target:{value}}) => setSearchText(value)}/>
+                <input
+                value = {searchText}
+                onChange = {({target:{value}}) => setSearchText(value)}
+                className="search-box-input"
+                />
             </label>
             <button>Buscar</button>
         </div>
