d5ab120cf962aea93f50ca81bf0dc50701a09127
dsrodriguezse <dsrodriguezse@unal.edu.co>
2023-01-12 01:59:05 -0500

    crea contenedor searchresultitem

--- buscador-personal/src/views/Search/components/SearchResults/index.js
+++ buscador-personal/src/views/Search/components/SearchResults/index.js
@@ -1,3 +1,5 @@
+import SearchResultsItem from "./SearchResultsItem"
+
 export default function SearchResults({results, isSearching}){
     return(
         <div style={{
@@ -5,20 +7,11 @@ export default function SearchResults({results, isSearching}){
             padding: "0rem 2rem 0rem 2rem"
         }}>
             {!results?.length && isSearching&&<p>No existen resultados</p>}
-            {results?.map((value)=>{
-                return(
-                    <div key = {value.id} style={{
-                        backgroundColor : "#E8E7E7",
-                        marginTop:"1rem",
-                        marginBottom:"1rem",
-                        padding:10,
-                        width:"100%"
-                    }}>
-                        <p>{value.name}</p>
-                        <p>{value.email}</p>
-                    </div>
-                )
-            })}
+            {results?.map((value)=>
+                <SearchResultsItem  
+                    key = {value.id} {...value}>
+                </SearchResultsItem>
+            )}
         </div>
     )
 }
