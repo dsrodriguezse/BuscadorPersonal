47fc9a2e2dbaf0b9ce13bc4c3341b173ab0a4775
dsrodriguezse <dsrodriguezse@unal.edu.co>
2023-01-12 05:10:32 -0500

    Información de la base de datos desde API

--- buscador-personal/src/views/Search/index.js
+++ buscador-personal/src/views/Search/index.js
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox";
 
-import data from "../../data/users.json"
 import "./style.css"
 import SearchResults from "./components/SearchResults";
 
@@ -9,6 +8,17 @@ export default function Search(){
     
     const [isAtTop, setIsAtTop] = useState(false)
     const [results, setResults] = useState([])
+    const [data, setData] = useState([])
+
+    useEffect(()=>{
+      const getUsers = async () =>{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+        setData(data) 
+      }  
+      
+      getUsers().catch(null)
+    },[])
 
     const handleCloseSearch=()=>{
         setIsAtTop(false)
